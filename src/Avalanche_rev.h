370d3f0858d905ee06c0d882a0622f9f17f95529
Chris Young <chris@unsatisfactorysoftware.co.uk>
2022-03-09 11:54:16 +0000

    Update docs/Tag as beta as having some issues on OS3.2.1

--- src/Avalanche_rev.h
+++ src/Avalanche_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
 #define REVISION	4
 #define DATE		"9.3.2022"
-#define VERS		"Avalanche 1.4"
-#define VSTRING		"Avalanche 1.4 (9.3.2022)\r\n"
-#define VERSTAG		"\0$VER: Avalanche 1.4 (9.3.2022)"
+#define VERS		"Avalanche 1.4beta"
+#define VSTRING		"Avalanche 1.4beta (9.3.2022)\r\n"
+#define VERSTAG		"\0$VER: Avalanche 1.4beta (9.3.2022)"
