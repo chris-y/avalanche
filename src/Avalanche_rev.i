22fe97e8569c0b422df5641c9262415bd617025f
Chris Young <chris@unsatisfactorysoftware.co.uk>
2022-08-03 12:10:55 +0100

    Bump version for release

--- src/Avalanche_rev.i
+++ src/Avalanche_rev.i
@@ -1,18 +1,18 @@
 VERSION		EQU	1
-REVISION	EQU	6
+REVISION	EQU	7
 
 DATE	MACRO
-		dc.b '31.5.2022'
+		dc.b '3.8.2022'
 		ENDM
 
 VERS	MACRO
-		dc.b 'Avalanche 1.6'
+		dc.b 'Avalanche 1.7'
 		ENDM
 
 VSTRING	MACRO
-		dc.b 'Avalanche 1.6 (31.5.2022)',13,10,0
+		dc.b 'Avalanche 1.7 (3.8.2022)',13,10,0
 		ENDM
 
 VERSTAG	MACRO
-		dc.b 0,'$VER: Avalanche 1.6 (31.5.2022)',0
+		dc.b 0,'$VER: Avalanche 1.7 (3.8.2022)',0
 		ENDM
--- src/Avalanche_rev.s
+++ src/Avalanche_rev.s
@@ -1,21 +1,21 @@
 VERSION = 1
-REVISION = 6
+REVISION = 7
 
 .macro DATE
-.ascii "31.5.2022"
+.ascii "3.8.2022"
 .endm
 
 .macro VERS
-.ascii "Avalanche 1.6"
+.ascii "Avalanche 1.7"
 .endm
 
 .macro VSTRING
-.ascii "Avalanche 1.6 (31.5.2022)"
+.ascii "Avalanche 1.7 (3.8.2022)"
 .byte 13,10,0
 .endm
 
 .macro VERSTAG
 .byte 0
-.ascii "$VER: Avalanche 1.6 (31.5.2022)"
+.ascii "$VER: Avalanche 1.7 (3.8.2022)"
 .byte 0
 .endm
