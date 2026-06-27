094938cd0d3dfcbba421599d6144587634220927
Chris Young <chris@unsatisfactorysoftware.co.uk>
2026-06-27 14:43:02 +0100

    Add beta tag to version string

--- src/Avalanche_rev.h
+++ src/Avalanche_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		3
 #define REVISION	4
 #define DATE		"26.6.2026"
-#define VERS		"Avalanche 3.4"
-#define VSTRING		"Avalanche 3.4 (26.6.2026)\r\n"
-#define VERSTAG		"\0$VER: Avalanche 3.4 (26.6.2026)"
+#define VERS		"Avalanche 3.4 beta"
+#define VSTRING		"Avalanche 3.4 beta (26.6.2026)\r\n"
+#define VERSTAG		"\0$VER: Avalanche 3.4 beta (26.6.2026)"
